901fddda9398fd26270acb011360c7d19cf74064
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-07-09 10:29:22 +0500

    add item view and controller

--- ItIsMarvel/Controllers/MarvelItemViewController.swift
+++ ItIsMarvel/Controllers/MarvelItemViewController.swift
@@ -10,22 +10,55 @@ import UIKit
 class MarvelItemViewController: UIViewController {
 
     var marvelItem: CharacterResult?
+    var seriesTable: UITableView!
+    
+    let testArray: [String] = Array(repeating: "new", count: 30)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let marvelItemView = MarvelItemView()
+        marvelItemView.seriesTableView.delegate = self
+        marvelItemView.seriesTableView.dataSource = self
+        
+        self.view.addSubview(marvelItemView.mainView)
+        setMarvelViewConstraints(for: marvelItemView.mainView)
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Constraints
+    func setMarvelViewConstraints(for marvelView: UIView) {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        let margins = view.layoutMarginsGuide
+        
+        var constraints = [
+            marvelView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            marvelView.trailingAnchor.constraint(equalTo: margins.trailingAnchor)]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        constraints = [
+            marvelView.topAnchor.constraint(equalToSystemSpacingBelow: safeAreaGuide.topAnchor, multiplier: 1.0),
+            marvelView.bottomAnchor.constraint(equalToSystemSpacingBelow: safeAreaGuide.bottomAnchor, multiplier: 1.0)]
+        
+        NSLayoutConstraint.activate(constraints)
     }
-    */
 
+}
+
+extension MarvelItemViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MarvelItemView.cellRegister, for: indexPath)
+        cell.textLabel?.text = testArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
