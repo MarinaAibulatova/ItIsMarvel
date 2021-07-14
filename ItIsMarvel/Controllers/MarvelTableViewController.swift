136b95778a4b943315f7f667caca1d16b045e56d
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-07-14 16:07:47 +0500

    add error message

--- ItIsMarvel/Controllers/MarvelTableViewController.swift
+++ ItIsMarvel/Controllers/MarvelTableViewController.swift
@@ -131,7 +131,12 @@ class MarvelTableViewController: UIViewController, MarvelCharactersManagerDelega
     
     //MARK: - MarvelRequestManagerDelegate
     func didFinishedWithError(error: String) {
-        print(error)
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error!", message: error, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
     func didFinishedFetchCharacters(result: [CharacterResult]) {
